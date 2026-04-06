3fbe0b6b348edf1432e6a5fa374686ae308e1776
Aleksey Treskov <alekseytreskov@Alekseys-MacBook-Air.local>
2026-04-06 00:13:33 -0600

    fix: correct coverage directory path for CI

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: 'node',
-  coverageDirectory: '../coverage/extension',
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'utils/**/*.js',
     '!utils/**/*.test.js'
